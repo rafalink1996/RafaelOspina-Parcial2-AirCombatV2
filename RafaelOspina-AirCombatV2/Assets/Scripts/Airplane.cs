11d221944ce5645c258b324c80ed11f41e933664
Rafael Ospina <rafaelospiname@gmail.com>
2018-04-16 18:04:09 -0500

    Parcial 2 terminado

---
+++ RafaelOspina-AirCombatV2/Assets/Scripts/Airplane.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour {
+	public float speed;
+	public float forwardSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (0, 0, forwardSpeed * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.Translate (0, speed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.Translate (0, -speed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.Translate (-speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.Translate (speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			GameObject missile = GameObject.Instantiate (Resources.Load ("Prefabs/Missile") as GameObject);
+			missile.transform.SetParent (this.gameObject.transform);
+			missile.transform.localPosition = new Vector3 (0.347f, -0.032f, -0.233f);
+			missile.transform.SetParent (null);
+			GameObject missile2 = GameObject.Instantiate (Resources.Load ("Prefabs/Missile") as GameObject);
+			missile2.transform.SetParent (this.gameObject.transform);
+			missile2.transform.localPosition = new Vector3 (-0.347f, -0.032f, -0.233f);
+			missile2.transform.SetParent (null);
+		}
+	}
+	/*void OnCollisionEnter(Collision collision) {
+		GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/Explosion") as GameObject);
+		explosion.transform.position = this.transform.position;
+		Destroy (this.gameObject);
+	}*/
+}
---
+++ RafaelOspina-AirCombatV2/Assets/Scripts/Missile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (0, 0, 50.0f * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		if (collision.collider.gameObject.tag == "MarsAlien") {
+
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/MarsExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+			Destroy (collision.collider.gameObject);
+			Destroy (this.gameObject);
+		}
+		if (collision.collider.gameObject.tag == "JupiterAlien") {
+
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/JupiterExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+			Destroy (collision.collider.gameObject);
+			Destroy (this.gameObject);
+		}
+		if (collision.collider.gameObject.tag == "NeptuneAlien") {
+
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/NeptuneExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+			Destroy (collision.collider.gameObject);
+			Destroy (this.gameObject);
+		}
+
+		if (collision.collider.gameObject.tag == "Fire") {
+
+			//GameObject son = collision.collider.gameObject.transform.Find ("LuzVerde").gameObject;
+			//son.GetComponent <Light> ().color = new Color (0, 0, 1);
+			Destroy (this.gameObject);
+
+		}
+	}
+}
